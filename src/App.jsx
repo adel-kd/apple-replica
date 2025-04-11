c6498363262064d1513821a49424ac152f861d8e
adel-kd <adelkedir971@gmail.com>
2025-04-11 05:49:59 -0500

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import "./component/css/bootstrap.css";
 import "./component/css/styles.css";
-import Header from "./component/header/header";
+import Header from "./component/Header/Header";
 import Section1 from "./component/Section1/section1";
 import Section2 from "./component/Section2/Section2";
 import Section3 from "./component/Section3/Section3";
 import Section4 from "./component/Section4/Section4";
 import Section5 from "./component/Section5/Section5";
 import Section6 from "./component/Section6/Section6";
-import Footer from "./component/footer/Footer";
+import Footer from "./component/Footer/Footer";
 function App() {
   const [count, setCount] = useState(0);
 
